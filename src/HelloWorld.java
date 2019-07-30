aa256f949799fc7d4fa07d5b43ce05711bcb941b
Moon <Moon@192.168.0.130>
2019-07-30 20:03:18 +0900

    git test 1 메시지 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Good to see you again");
+		System.out.println("Git Test 1");
 	}	
 }
